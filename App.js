eeed5456a7d2fad4d50b188add1645db32f21f7d
unknown <agswami2@gmail.com>
2021-04-23 21:02:09 -0700

    test login

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase'
+import firebaseConfig  from './config'
+firebase.initializeApp(firebaseConfig)
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>hello</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyApObIV9qOoZMhF1OtmHEon20AApUWEHLA",
+    authDomain: "react-native-learn-e5f17.firebaseapp.com",
+    projectId: "react-native-learn-e5f17",
+    storageBucket: "react-native-learn-e5f17.appspot.com",
+    messagingSenderId: "549286068821",
+    appId: "1:549286068821:web:91cec476b1ca1a447bcadc",
+    measurementId: "G-Y277HREWVD"
+  };
